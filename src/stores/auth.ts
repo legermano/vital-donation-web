b44c3cc93c12edf250c510228afcdfda2c751e19
Lucas Germano <lucas.emanuel.germano@gmail.com>
2022-10-04 23:53:00 -0300

    Implementing login logic

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -3,22 +3,26 @@ import { defineStore } from "pinia";
 import { useStorage } from "@vueuse/core";
 import { router } from "@/router";
 import { useNotificationStore } from "@/stores";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import type { IAuth } from "@/interfaces";
 
 export const useAuthStore = defineStore("auth", () => {
   // Store JWT token and refresh in local storage to keep user logged in between page refreshes
+  // TODO: Verify why does not store correctly on reload
   const auth = useStorage("auth", null as IAuth | null);
   const returnUrl = ref<string | null>();
 
   const isLoggedIn = computed(() => !!auth.value?.access_token);
 
   const login = async (cpf: string, password: string): Promise<void> => {
+    const data = new FormData();
+    data.append("login", cpf.replace(/[^\d]+/g, ""));
+    data.append("password", password);
+
     axios
-      .get("http://localhost:8080/login", {
-        data: {
-          login: cpf,
-          password,
+      .post("/login", data, {
+        headers: {
+          "Content-Type": "multipart/form-data;",
         },
       })
       .then(({ data }) => {
@@ -33,7 +37,17 @@ export const useAuthStore = defineStore("auth", () => {
       .catch((error) => {
         if (error instanceof Error) {
           const notificationStore = useNotificationStore();
-          notificationStore.error("Ocoreu um erro", error.message);
+
+          if (error instanceof AxiosError && error.response?.status == 403) {
+            notificationStore.error(
+              "Erro ao logar!",
+              "Certifique-se que digitou corretamente seu CPF e senha."
+            );
+
+            return;
+          }
+
+          notificationStore.error("Erro ao logar!", error.message);
         }
       });
   };
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default ({ node }) => {
+  process.env = { ...process.env, ...loadEnv(node, process.cwd()) };
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-});
+    server: {
+      proxy: {
+        "/login": {
+          target: process.env.VITE_API_URL + "/login",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/login/, ""),
+        },
+        "/api": {
+          target: process.env.VITE_API_URL + "/api",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
